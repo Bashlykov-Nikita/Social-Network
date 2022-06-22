352ee5349572396661a6e3efbb4e6b8070e14505
Nikita <bashlykov75@gmail.com>
2022-06-22 11:59:34 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://seeklogo.com/images/B/business-company-logo-C561B48365-seeklogo.com.png' alt='logo_img' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProfilePage = () => {
+    return (
+        <div className='content'>
+            <img src='https://images.pexels.com/photos/2131614/pexels-photo-2131614.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='content-img' />
+            Main content
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    NewPost
+                </div>
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    );
+}
+
+export default ProfilePage;
